4aa6a666d056bea284cd1fc23368286c8a1449a5
Mhar Andal <mhar.andal@gamut.com>
2018-03-17 15:10:52 -0500

    Adds CleanWebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const paths = {
     DIST: path.resolve(__dirname, 'dist'), // bundle file path
     SRC: path.resolve(__dirname, 'src'), // source folder path ->    
-    JS: path.resolve(__dirname, 'src/js'), // js
 };
+
+// paths that should be cleaned
+let pathsToClean = [
+    'dist',
+    'build'
+];
+
+let cleanOptions = {
+    verbose: true,
+    dry: false
+}
 
 module.exports = {
     entry: './index.js',
@@ -20,6 +31,18 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(paths.SRC, 'index.html'),
-        }),
+        }), // serves up html
+        new CleanWebpackPlugin(pathsToClean, cleanOptions), // clean ./build
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ]
+    }
 };
