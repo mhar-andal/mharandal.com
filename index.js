97aab4fae1a077ac57154612189fbe1a6472a715
Mhar Andal <mhar.andal@gamut.com>
2018-03-17 15:13:38 -0500

    Adds React to project

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from react es6</h1>
+            </div>
+        );
+    }
+}
+render(<Hello />, document.getElementById('root'));
